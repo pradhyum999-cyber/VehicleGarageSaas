3680b791a060400a6140b6da976c0e4086536ce9
pradhyum999-cyber <pradhum101@gmail.com>
2025-03-08 02:05:13 +0530

    Updates

---
+++ precache-manifest.791ccdc2338fc4799674c82c5aac84d9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "457d7b8fea167bd10699",
+    "url": "/VehicleGarageSaas/static/js/0.86a2851e.chunk.js"
+  },
+  {
+    "revision": "832973b8edc4fb2185ab",
+    "url": "/VehicleGarageSaas/static/js/1.85741399.chunk.js"
+  },
+  {
+    "revision": "6804c594fb38764a2b89",
+    "url": "/VehicleGarageSaas/static/js/main.4baebb1f.chunk.js"
+  },
+  {
+    "revision": "0827ebd7a3ff14717c9f",
+    "url": "/VehicleGarageSaas/static/js/runtime~main.0bf7ae03.js"
+  },
+  {
+    "revision": "ff8a6ecfad3627d3a6e1",
+    "url": "/VehicleGarageSaas/static/js/4.1596d0c0.chunk.js"
+  },
+  {
+    "revision": "7c624b68172e0d9b7416",
+    "url": "/VehicleGarageSaas/static/js/5.04e80107.chunk.js"
+  },
+  {
+    "revision": "631cc1dcb2e6d8b0cc43",
+    "url": "/VehicleGarageSaas/static/js/6.a4ad4acd.chunk.js"
+  },
+  {
+    "revision": "d4036bc86b04ce09aa2b",
+    "url": "/VehicleGarageSaas/static/css/7.7af9310c.chunk.css"
+  },
+  {
+    "revision": "d4036bc86b04ce09aa2b",
+    "url": "/VehicleGarageSaas/static/js/7.49eb6162.chunk.js"
+  },
+  {
+    "revision": "00d2e63362b4c330b7e9",
+    "url": "/VehicleGarageSaas/static/css/8.2d677d45.chunk.css"
+  },
+  {
+    "revision": "00d2e63362b4c330b7e9",
+    "url": "/VehicleGarageSaas/static/js/8.46bf2bff.chunk.js"
+  },
+  {
+    "revision": "f748020491a69d5da848",
+    "url": "/VehicleGarageSaas/static/js/9.417c531f.chunk.js"
+  },
+  {
+    "revision": "9bf9d7128de546a169c9",
+    "url": "/VehicleGarageSaas/static/js/10.4728d2a2.chunk.js"
+  },
+  {
+    "revision": "b100eefd12bf3e4fbf61",
+    "url": "/VehicleGarageSaas/static/js/11.042e10a1.chunk.js"
+  },
+  {
+    "revision": "3f602ec3f5668b791a71",
+    "url": "/VehicleGarageSaas/static/js/12.ed31f552.chunk.js"
+  },
+  {
+    "revision": "d85e16f6315b3b8014a0",
+    "url": "/VehicleGarageSaas/static/js/13.c80a39bc.chunk.js"
+  },
+  {
+    "revision": "74dee0ceba1de0fe2a4e",
+    "url": "/VehicleGarageSaas/static/js/14.9db03ad3.chunk.js"
+  },
+  {
+    "revision": "3f258cc23ec6401c7eda",
+    "url": "/VehicleGarageSaas/static/js/15.8e037eea.chunk.js"
+  },
+  {
+    "revision": "24c69bb3bba1f0280cf7",
+    "url": "/VehicleGarageSaas/static/js/16.92a63e5f.chunk.js"
+  },
+  {
+    "revision": "0df48b0f7b5735a2be36",
+    "url": "/VehicleGarageSaas/static/js/17.34faeac6.chunk.js"
+  },
+  {
+    "revision": "d70653058ca13bb5c8fe",
+    "url": "/VehicleGarageSaas/static/js/18.65f03904.chunk.js"
+  },
+  {
+    "revision": "3e5a25a5531af5fec131",
+    "url": "/VehicleGarageSaas/static/js/19.feaa9cdc.chunk.js"
+  },
+  {
+    "revision": "2256de2ee775b26f5e50",
+    "url": "/VehicleGarageSaas/static/js/20.82b608eb.chunk.js"
+  },
+  {
+    "revision": "27f6dcdabde78044dd32a8882a52bbd5",
+    "url": "/VehicleGarageSaas/index.html"
+  }
+];
