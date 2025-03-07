532d11231eb7b8551d28b819124cfc84ee180c77
pradhyum999-cyber <pradhum101@gmail.com>
2025-03-08 01:55:05 +0530

    fix

--- src/logged_out/components/blog/BlogCard.js
+++ src/logged_out/components/blog/BlogCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import format from "date-fns/format";
 import classNames from "classnames";
 import { Typography, Card, Box } from "@mui/material";
 import { format } from 'date-fns';
